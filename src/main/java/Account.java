e2b0a3c10cd2b9956459e605fe13b934813cde92
Munir Sanzar <munir@zipcodes-MacBook-Pro-9.local>
2020-11-13 18:50:35 -0500

    Update Munir

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,2 +1,24 @@
+import com.sun.deploy.net.MessageHeader;
+
 public class Account {
+    public Integer accountNumber;
+    public Integer ownerID;
+    public Double balance;
+
+    public Account(Double balance, Integer ownerID, Integer accountNumber){
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+        this.ownerID = ownerID;
+    }
+    public Integer getAccountNumber() {
+        return accountNumber;
+    }
+
+    public Integer getOwnerID() {
+        return ownerID;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
 }
