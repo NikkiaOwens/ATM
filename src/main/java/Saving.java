02bc6d217e3e33c47c0e370b57f4d1921507f0e9
TomCorson <corsontom@yahoo.com>
2020-11-15 13:46:11 -0500

    delete savings intrest

--- src/main/java/Saving.java
+++ src/main/java/Saving.java
@@ -1,15 +1,8 @@
 import com.sun.deploy.net.MessageHeader;
 
 public class Saving extends Account {
-    public Double interestR;
 
     public Saving(){
 
-    }
-
-
-
-    public Double getInterestR(){
-        return this.interestR;
     }
 }
