90d74b38623d376a73cb07887db9224a4eeefea4
TomCorson <corsontom@yahoo.com>
2020-11-15 13:36:27 -0500

    savngs menu test

--- src/main/test/SavingsMenuTest.java
+++ src/main/test/SavingsMenuTest.java
@@ -1,5 +1,27 @@
 import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class SavingsMenuTest {
+
+
+    @Test
+    public void setTransactionHistoryTest(){
+        SavingsMenu savingsMenu = new SavingsMenu();
+        String expected = "this is a test";
+        savingsMenu.setTransactionHistory(expected);
+        String actual = savingsMenu.getTransactionHistory();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getTransactionHistoryTest(){
+        SavingsMenu savingsMenu = new SavingsMenu();
+        String expected = "this is a much different test";
+        savingsMenu.setTransactionHistory(expected);
+        String actual = savingsMenu.getTransactionHistory();
+        assertEquals(expected,actual);
+    }
 
 }
