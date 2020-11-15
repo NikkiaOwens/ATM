3627415657a5f4eb62bdf71d5f8454e161494f81
TomCorson <corsontom@yahoo.com>
2020-11-15 13:46:37 -0500

    delete stuff

--- src/main/java/Investment.java
+++ src/main/java/Investment.java
@@ -1,19 +1,7 @@
 public class Investment extends Account {
-    Double risk;
+  
     public Investment(){
 
-    }
-
-
-
-
-
-    public Double getRisk() {
-        return risk;
-    }
-
-    public void setRisk(Double risk) {
-        this.risk = risk;
     }
 
 }
